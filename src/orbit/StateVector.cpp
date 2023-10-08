aded030d791260d61578b8a64c7f05e1dda0d507
Gabriel Valles <gvallesv@outlook.es>
2023-10-08 10:15:41 +0200

    Implemented conversion sv to orbital elements

--- src/orbit/StateVector.cpp
+++ src/orbit/StateVector.cpp
@@ -1,5 +1,6 @@
 #include "StateVector.hpp"
 #include "OrbitalElements.hpp"
+#include <bodies/Body.hpp>
 
 StateVector::StateVector(double rx, double ry, double rz, double vx, double vy, double vz) 
     : sv{math::vector{rx, ry, rz, vx, vy, vz}} {
@@ -9,7 +10,48 @@ StateVector::StateVector(math::vector sv)
     : sv{sv} {
 }
 
-OrbitalElements StateVector::to_oe() {
-    OrbitalElements oe{0,0,0,0,0,0};
-    return OrbitalElements;
+OrbitalElements StateVector::to_oe(Body central) {
+    math::vector r_vec = sv.subvec(0,3);
+    math::vector v_vec = sv.subvec(3,6);
+    double r = norm(r_vec);
+    double v = norm(v_vec);
+    // Compute angular momentum 
+    math::vector h_vec = cross(r_vec, v_vec);
+    double h = norm(h_vec);
+    // Retrieve mu
+    double mu = central.get_mu();
+    // Compute semi-major axis
+    double a = 1 / (-pow(v, 2)/mu + 2/r);
+    // Compute eccentricity
+    math::vector e_vec = cross(v_vec, h_vec)/mu - r_vec/r;
+    double e = norm(e_vec);
+
+    // Make use of perifocal frame
+    double i, raan, aop, ta;
+    math::vector ip, jp, kp, ur;
+    kp = h_vec/h;
+    i = acos(kp[2]);   // Compute inclination
+    if (i == 0) {
+        raan = 0; // Zero-inclination exception
+    } else {
+        raan = atan2(kp[0], -kp[1]); // Right ascension of ascending node
+    }
+    if (e==0) {
+        ip = math::vector{cos(raan), sin(raan), 0}; // Circular orbit exception
+    } else {
+        ip = e_vec/e; // Perifocal X-axis
+    }
+    jp = cross(kp, ip);
+    // Define radial unary vector
+    ur = r_vec/r;
+    // Compute argument of periapsis
+    if (i==0) {
+        math::vector raan_vec{cos(raan), sin(raan), 0};
+        aop = atan2(dot(cross(raan_vec, ip), kp), dot(raan_vec, ip));
+    } else {
+        aop = atan2(ip[2], jp[2]);
+    }
+    ta = atan2(dot(cross(ip, ur), kp), dot(ip, ur));
+
+    return OrbitalElements{a, e, raan, i, aop, ta};
 }
--- src/orbit/StateVector.hpp
+++ src/orbit/StateVector.hpp
@@ -1,4 +1,5 @@
 #include <math/Vector.hpp>
+#include <bodies/Body.hpp>
 
 #ifndef _STATE_VECTOR_HPP_
 #define _STATE_VECTOR_HPP_
@@ -10,7 +11,7 @@ class StateVector {
 public:
     StateVector(double rx, double ry, double rz, double vx, double vy, double vz);
     StateVector(math::vector sv);
-    OrbitalElements to_oe();
+    OrbitalElements to_oe(Body central);
 };
 
 #endif //_STATE_VECTOR_HPP_
