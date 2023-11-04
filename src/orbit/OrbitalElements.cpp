6cbc95b33269d5315387878405f1747f67107ad0
Gabriel Valles <gvallesv@outlook.es>
2023-11-04 20:16:03 +0100

    Orbital plane state vector. Rotation yet to be implemented

--- src/orbit/OrbitalElements.cpp
+++ src/orbit/OrbitalElements.cpp
@@ -12,9 +12,37 @@ math::vector OrbitalElements::get_oe() {
     return oe;
 }
 
-StateVector OrbitalElements::to_sv(Body) {
-    StateVector sv{0,0,0,0,0,0};
-    return sv;
+StateVector OrbitalElements::to_sv(Body body) {
+    double a = oe.at(0);
+    double e = oe.at(1);
+    double raan = oe.at(2);
+    double i = oe.at(3);
+    double aop = oe.at(4);
+    double ta = oe.at(5);
+
+    // Orbital plane
+    double r = a*(1-pow(e,2)) / (1+e*cos(ta));  // scalar r and v
+    double v = sqrt(body.get_mu()*(2/r - 1/a));
+    math::vector ur{cos(ta), sin(ta), 0}; // polar coordinates unary vectors
+    math::vector ut{-sin(ta), cos(ta), 0};
+    auto r_orb = r*ur; // position vector
+    double gamma = atan2(e*sin(ta), 1+e*cos(ta)); // flight path angle
+    auto uv = cos(gamma)*ut + sin(gamma)*ur;
+    auto v_orb = v*uv; // velocity vector
+
+    // Rotate from orbital plane orientation
+
+    auto r_vec = r_orb;
+    auto v_vec = v_orb;
+
+    return StateVector{
+        r_vec.at(0),
+        r_vec.at(1),
+        r_vec.at(2),
+        v_vec.at(0),
+        v_vec.at(1),
+        v_vec.at(2)
+    };
 }
 
 bool OrbitalElements::operator==(const OrbitalElements& obj) {
