c4d62893a04ae88eb091db3b8ccbe8b6837d023b
Gabriel Valles <gvallesv@outlook.es>
2023-11-05 21:44:28 +0100

    Modified Rotation.hpp to define pasive rotation matrices

--- src/math/tests/TestRotation.cpp
+++ src/math/tests/TestRotation.cpp
@@ -7,15 +7,15 @@ void assert_near_vector(math::vector v1, math::vector v2, double tol) {
     ASSERT_NEAR(norm(v1-v2), 0, tol);
 }
 
-TEST(RotationTest, RotationMatrix) {
-    math::vector v{1, -3, 4};
-    double angle = M_PI/6;
+TEST(RotationTest, RotationMatrix2) {
+    math::vector v{1, 0, 0};
+    math::vector v2{0, 1, 0};
 
-    math::vector v_xrot{1, -4.598076211353, 1.964101615138};
-    math::vector v_yrot{2.866025403784, -3, 2.964101615138};
-    math::vector v_zrot{2.366025403784, -2.098076211353, 4};
+    math::vector v_rotz{cos(M_PI/6), -sin(M_PI/6), 0};
+    math::vector v_roty{cos(M_PI/6), 0, sin(M_PI/6)};
+    math::vector v2_rotx{0, cos(M_PI/6), -sin(M_PI/6)};
 
-    assert_near_vector(R1(angle)*v, v_xrot, 1e-12);
-    assert_near_vector(R2(angle)*v, v_yrot, 1e-12);
-    assert_near_vector(R3(angle)*v, v_zrot, 1e-12);
+    ASSERT_EQ(R3(M_PI/6)*v, v_rotz);
+    ASSERT_EQ(R2(M_PI/6)*v, v_roty);
+    ASSERT_EQ(R1(M_PI/6)*v2, v2_rotx);
 }
